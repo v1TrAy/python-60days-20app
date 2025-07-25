b8d3ce8138e51076b5ee59bb01fb43c89e145a6e
v1TrAy <v1TrAy@gmail.com>
2025-07-25 16:28:11 +0330

    Reorganized folders and files: moved App 1 into structured module directory

---
+++ 01 - MODULE 1) PYTHON BASICS/App 1 - Build a Todo List App/main.py
@@ -0,0 +1,67 @@
+# from functions import get_todos, write_todos
+import functions
+
+while True:
+    user_prompt = "Type add, edit, show or exit or complete:"
+    user_action = input(user_prompt)
+    # Removes leading and trailing whitespace
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo + '\n')
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = functions.get_todos()
+
+            new_todo = input("Enter New Todo:")
+            todos[number] = new_todo + '\n'
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("Your Command Is Not Valid!!")
+            continue
+
+    elif user_action.startswith('show'):
+
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"{index+1}-{item}")
+
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message = f'Todo {todo_to_remove} was removed from the list.'
+            print(message)
+        except IndexError:
+            print("There is no item with that number!")
+            continue
+
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("Command is not valid!")
+
+print("Cya!")
