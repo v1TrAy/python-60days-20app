199f645d58c72c0922f0ed5c61fb530a89d97554
v1TrAy <v1TrAy@gmail.com>
2025-07-27 13:58:12 +0330

    MODULE 1: Day 17, Up to video 3

--- 01 - MODULE 1) PYTHON BASICS/App 1 - Build a Todo List App/gui.py
+++ 01 - MODULE 1) PYTHON BASICS/App 1 - Build a Todo List App/gui.py
@@ -2,10 +2,47 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter Todo")
+input_box = sg.InputText(tooltip="Enter Todo", key="todo")
 add_botton = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_botton]])
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
 
-window.read()
+edit_button = sg.Button("Edit")
+
+window = sg.Window(
+    'My To-Do App',
+    layout=[[label], [input_box, add_botton], [list_box, edit_button]],
+    font=("helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window["todos"].Update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values['todo']
+
+            todos = functions. get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].Update(values=todos)
+
+        case "todos":
+            window["todo"].Update(value=values["todos"][0])
+
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
