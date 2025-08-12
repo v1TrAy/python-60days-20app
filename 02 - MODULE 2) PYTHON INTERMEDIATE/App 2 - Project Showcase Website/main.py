e87d81264cd3a2381997a5755c163dca97dd6932
v1TrAy <v1TrAy@gmail.com>
2025-08-12 08:20:26 +0330

    MODULE 2: Day 22, Up to video 6

--- 02 - MODULE 2) PYTHON INTERMEDIATE/App 2 - Project Showcase Website/main.py
+++ 02 - MODULE 2) PYTHON INTERMEDIATE/App 2 - Project Showcase Website/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -19,4 +19,23 @@ Reigniting the spark, one line of code at a time.
 content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
-st.write(content)
+st.write(content2)
+
+col3, emptry_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
